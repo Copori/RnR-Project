32fd02f28c0c958f75370800402c7eb50ba10238
jaeminLee524 <rkwoals524@hanmail.net>
2022-01-17 20:50:47 +0900

    회원가입:email추가, 회원수정:암호화, 회원정보삭제 추가

--- src/main/java/com/example/bookrecommend/service/UserService.java
+++ src/main/java/com/example/bookrecommend/service/UserService.java
@@ -6,6 +6,7 @@ import com.example.bookrecommend.domain.User;
 import com.example.bookrecommend.repository.UserRepository;
 import com.example.bookrecommend.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,16 +17,18 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional
     public User signup(UserDto userDto) {
         if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
             throw new RuntimeException("이미 가입되어 있는 유저입니다.");
         }
 
-        // param으로 받은 user로 권한정보 생성, 가입은 UESR로만
+        // param으로 받은 user로 권한정보 생성, 가입은 USER로만
         Authority authority = Authority.builder()
                 .authorityName("ROLE_USER")
                 .build();
@@ -34,7 +37,8 @@ public class UserService {
         User user = User.builder()
                 .username(userDto.getUsername())
                 .password(passwordEncoder.encode(userDto.getPassword()))
-                .nickname(userDto.getNickname())
+                .email(userDto.getEmail())
+//                .nickname(userDto.getNickname())
                 .authorities(Collections.singleton(authority))
                 .activated(true)
                 .build();
@@ -55,25 +59,44 @@ public class UserService {
     /** 회원정보 수정 */
     @Transactional
     public void update(Long id, UserDto request) {
+
+        log.info("request : {}", request);
+
+        // 요청 객체 추출
         Optional<User> findUser = userRepository.findById(id);
+
+        // 추출한 객체에 수정 요청으로 들어온 값 세팅
         if (findUser.isPresent()) {
             User user = findUser.stream().findFirst().get();
             user.setUserId(id);
             user.setUsername(request.getUsername());
-            //TODO 시큐리티에서 회원정보 어떻게 하는지 =>
-            user.setPassword(request.getPassword());
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
             user.setEmail(request.getEmail());
         }
     }
 
     /** 회원 단건 조회 */
     public User findById(Long id) {
-        Optional<User> findUser = userRepository.findById(id);
+        Optional<User> findUser = userRepository.findByIdAndActivatedTrue(id);
 
         User returnUser = findUser.stream().findFirst().orElseGet(() -> {
             return new User();
         });
 
         return returnUser;
+    }
+
+    /** 회원 정보 삭제 */
+    @Transactional
+    public void deleteUser(Long id) {
+        Optional<User> findUser = userRepository.findByIdAndActivatedTrue(id);
+
+        //삭제할 유저가 존재할 경우
+        if( findUser.isPresent() ) {
+            User user = findUser.stream().findFirst().get();
+            user.setActivated(false);
+
+            userRepository.save(user);
+        }
     }
 }
