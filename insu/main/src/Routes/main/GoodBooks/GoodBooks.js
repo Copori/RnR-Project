973455a06fe7006ba6ad43913568e0445c5d396e
iinsue <hinsu93@naver.com>
2022-01-18 17:16:19 +0900

    api 여러번 호출해서 가져오기

--- insu/main/src/Routes/main/GoodBooks/GoodBooks.js
+++ insu/main/src/Routes/main/GoodBooks/GoodBooks.js
@@ -13,8 +13,8 @@ function GoodBooks() {
   //API variables
   const API_URL = "http://api.kcisa.kr/openapi/service/rest/meta4/getKCPG0506";
   const SECRET_KEY = process.env.REACT_APP_RECOMMEND_SERVICE_KEY;
-  let pageNum = 1; //total books = 825
-  let countBook = 825; // 불러올 책의 개수
+  let pageNum = 0; //total books = 825
+  let countBook = 5; // 불러올 책의 개수
 
   //icon variablaes
   const [bookToggle, setBookToggle] = useState(true);
@@ -31,12 +31,27 @@ function GoodBooks() {
   //react variables
   const [loading, setLoading] = useState(true);
   const [books, setBooks] = useState([]);
+  const [booklists, setBookLists] = useState([]);
 
   //API를 읽어서 json에 담는 함수
   const getBooks = async () => {
+    let bookList = [];
+    for(let i = 1; i<10; i++){
+      const json = await ( await fetch(
+        `${API_URL}?serviceKey=${SECRET_KEY}&numOfRows=${countBook}&pageNo=${pageNum + i}`,
+        {
+          headers: {
+            accept: "application/json",
+          },
+        }
+      )
+    ).json();
+      bookList[i-1] = bookList.concat(json.response.body.items.item);
+      console.log(bookList[i-1]);
+    }
     const json = await (
       await fetch(
-        `${API_URL}?serviceKey=${SECRET_KEY}&numOfRows=${countBook}&pageNo=${pageNum}`,
+        `${API_URL}?serviceKey=${SECRET_KEY}&numOfRows=825&pageNo=1`,
         {
           headers: {
             accept: "application/json",
@@ -45,15 +60,17 @@ function GoodBooks() {
       )
     ).json();
     setBooks(json.response.body.items.item);
+    setBookLists(bookList);
+    console.log(bookList);
     setLoading(false);
   };
-
+  
   // useEffect
   useEffect(() => {
     getBooks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(books[0]);
+
   //View
   return (
     <div className="GoodBooks">
