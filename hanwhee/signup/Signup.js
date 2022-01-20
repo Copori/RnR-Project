f310b21daed71b5e34b1dac952059ed3d09dafe7
KimHanWhee <84313936+KimHanWhee@users.noreply.github.com>
2022-01-20 16:09:03 +0900

    Signup 백엔드 연동

--- hanwhee/signup/Signup.js
+++ hanwhee/signup/Signup.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import useForm from "./useForm";
 import validate from "./validate";
 import "./style/style.css";
+import axios from "axios";
 
 function Signup() {
+  let [id, setId] = useState("");
+  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+  let [message, setMessage] = useState("");
+  let [success, setSuccess] = useState(true);
+
   const { values, errors, submitting, handleChange, handleSubmit } = useForm({
-    initialValues: { id: "", email: "", password: "", passwordcheck: "" },
+    initialValues: { Vid: "", Vemail: "", Vpassword: "", Vpasswordcheck: "" },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -13,12 +20,30 @@ function Signup() {
   });
 
   return (
-    <form onSubmit={handleSubmit} noValidate>
-      <div className="signup">
-        <div className="signup__box">
-          <div className="signup__header">
-            <span className="signup__header--logo">Read&amp;Review</span>
-          </div>
+    <div className="signup">
+      <div className="signup__box">
+        <div className="signup__header">
+          <span className="signup__header--logo">Read&amp;Review</span>
+        </div>
+        <form
+          onSubmit={
+            (handleSubmit,
+            (e) => {
+              e.preventDefault();
+              axios
+                .post("http://localhost:8080/api/signup", {
+                  username: id,
+                  eamil: email,
+                  password: password,
+                })
+                .then(function (response) {
+                  console.log(response);
+                  // setSuccess(response.data.isSuccess);
+                  // setMessage(response.data.message);
+                });
+            })
+          }
+        >
           <div className="signup__body">
             <div className="signup__body--input">
               <span className="signup__body--id-title">아이디</span>
@@ -27,52 +52,67 @@ function Signup() {
                 name="id"
                 className="signup__body--input-id"
                 placeholder="아이디를 입력하세요."
-                value={values.id}
-                onChange={handleChange}
+                onChange={
+                  (handleChange,
+                  (e) => {
+                    console.log(e.target.value);
+                    setId(e.target.value);
+                  })
+                }
               />
-              {errors.id && (
-                <span className="signup__body--id-error">{errors.id}</span>
+              {errors.Vid && (
+                <span className="signup__body--id-error">{errors.Vid}</span>
               )}
+
               <span className="signup__body--email-title">이메일</span>
               <input
                 type="email"
                 name="email"
                 className="signup__body--input-email"
                 placeholder="이메일을 입력하세요."
-                value={values.email}
-                onChange={handleChange}
+                onChange={
+                  (handleChange,
+                  (e) => {
+                    setEmail(e.target.value);
+                  })
+                }
               />
-              {errors.email && (
+              {errors.Vemail && (
                 <span className="signup__body--email-error">
-                  {errors.email}
+                  {errors.Vemail}
                 </span>
               )}
+
               <span className="signup__body--pw-title">비밀번호</span>
               <input
                 type="password"
                 name="password"
                 className="signup__body--input-pw"
                 placeholder="비밀번호를 입력하세요."
-                value={values.password}
-                onChange={handleChange}
+                onChange={
+                  (handleChange,
+                  (e) => {
+                    setPassword(e.target.value);
+                  })
+                }
               />
-              {errors.password && (
+              {errors.Vpassword && (
                 <span className="signup__body--pw-error">
-                  {errors.password}
+                  {errors.Vpassword}
                 </span>
               )}
+
               <span className="signup__body--pwcheck-title">비밀번호 확인</span>
               <input
                 type="password"
                 name="passwordcheck"
                 className="signup__body--input-pwcheck"
                 placeholder="비밀번호를 다시 입력해주세요."
-                value={values.passwordcheck}
                 onChange={handleChange}
               />
-              {errors.passwordcheck && (
+              {errors.Vpasswordcheck && (
                 <span className="signup__body--pwck-error">
-                  {errors.passwordcheck}
+                  {errors.Vpasswordcheck}
                 </span>
               )}
             </div>
@@ -82,9 +122,9 @@ function Signup() {
               가입하기
             </button>
           </div>
-        </div>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
 
