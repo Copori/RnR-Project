24a95793577d4acdb943e72f48c97058a127c2d8
iinsue <hinsu93@naver.com>
2022-01-21 21:15:05 +0900

    login signup

--- insu/main/src/Routes/login/Login.js
+++ insu/main/src/Routes/login/Login.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
 import "../style/login/style.css";
-import {Link} from"react-router-dom";
+import React, { useState, useEffect } from "react";
+import {Link, useNavigate} from"react-router-dom";
+import axios from "axios";
 
 function Login() {
   const [inputId, setInputId] = useState("");
   const [inputPw, setInputPw] = useState("");
+  let [jwt, setJwt] = useState("");
+  const navigate = useNavigate();
 
   const handleInputId = (e) => {
     setInputId(e.target.value);
@@ -14,7 +17,35 @@ function Login() {
     setInputPw(e.target.value);
   };
 
-  const onClickLogin = () => {};
+  useEffect(() => {
+    localStorage.setItem("JWT", jwt);
+    console.log(jwt);
+  });
+
+  const onClickLogin = (e) => {
+    e.preventDefault();
+
+    if (inputId === "") {
+      alert("아이디를 입력하세요");
+    } else if (inputPw === "") {
+      alert("비밀번호를 입력하세요");
+    }
+
+    axios
+    .post("http://localhost:8080/api/login", {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+      username: inputId,
+      password: inputPw,
+    })
+    .then(function (response) {
+      console.log(response);
+      console.log(response.data.token);
+      setJwt(response.data.token);
+      navigate("/", { replace: true });
+    });
+};
 
   const onClickSignin = () => {};
 
--- insu/main/src/Routes/signUp/Signup.js
+++ insu/main/src/Routes/signUp/Signup.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import useForm from "./useForm";
+import { useNavigate } from "react-router-dom";
 import validate from "./validate";
 import "../style/signUp/style.css";
 import axios from "axios";
 
 function Signup() {
-  let [id, setId] = useState("");
+  let [username, setId] = useState("");
   let [password, setPassword] = useState("");
   let [email, setEmail] = useState("");
   let [message, setMessage] = useState("");
-  let [success, setSuccess] = useState(true);
+  let [pwCheck, setPwCheck] = useState("");
+
+  const [toggle, setToggle] = useState(false);
+
+  const navigate = useNavigate();
+
+
 
   const { values, errors, submitting, handleChange, handleSubmit } = useForm({
     initialValues: { Vid: "", Vemail: "", Vpassword: "", Vpasswordcheck: "" },
@@ -26,23 +33,39 @@ function Signup() {
           <span className="signup__header--logo">Read&amp;Review</span>
         </div>
         <form
-          onSubmit={
-            (handleSubmit,
-            (e) => {
-              e.preventDefault();
-              axios
-                .post("http://localhost:8080/api/signup", {
-                  username: id,
-                  eamil: email,
-                  password: password,
-                })
-                .then(function (response) {
-                  console.log(response);
-                  // setSuccess(response.data.isSuccess);
-                  // setMessage(response.data.message);
-                });
-            })
-          }
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            //유효성 검증
+            if (username === "") {
+              alert("아이디를 입력하세요.");
+            } else if (email === "") {
+              alert("이메일을 입력하세요.");
+            } else if (password === "") {
+              alert("비밀번호를 입력하세요.");
+            } else if (pwCheck === "") {
+              alert("비밀번호를 다시 입력해주세요.");
+            } else if (password !== pwCheck) {
+              alert("비밀번호가 일치하지 않습니다.");
+            } else {
+              if (toggle === false) {
+                axios
+                  .post("http://localhost:8080/api/signup", {
+                    username: username,
+                    password: password,
+                    email: email,
+                  })
+                  .then(function (response) {
+                    console.log("response" + response);
+                    navigate("/login", { replace: true });
+                    // setSuccess(response.data.isSuccess);
+                    // setMessage(response.data.message);
+                  });
+              } else {
+                alert("입력된 값이 유효하지 않습니다.");
+              }
+            }
+          }}
         >
           <div className="signup__body">
             <div className="signup__body--input">
