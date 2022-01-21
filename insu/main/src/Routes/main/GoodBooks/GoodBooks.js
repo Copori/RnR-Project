d73a39785d96def95dff7e7684c44fd1aab9d34e
iinsue <hinsu93@naver.com>
2022-01-21 20:08:48 +0900

    더 보기 수정 중

--- insu/main/src/Routes/main/GoodBooks/GoodBooks.js
+++ insu/main/src/Routes/main/GoodBooks/GoodBooks.js
@@ -8,6 +8,7 @@ import {
   GiSecretBook,
   GiBlackBook,
 } from "react-icons/gi";
+// import MoreBooks from "../../bookDetail/MoreBooks";
 
 function GoodBooks() {
   //API Constants
@@ -15,14 +16,26 @@ function GoodBooks() {
   const SECRET_KEY = process.env.REACT_APP_RECOMMEND_SERVICE_KEY;
   let pageNum = 0; //total books = 825
   let countBook = 5; // 불러올 책의 개수
-
+  
   //API variables
   const [loading, setLoading] = useState(true);
   const [books, setBooks] = useState([]);
   const [booklists, setBookLists] = useState([]);
+  const moreBooks = [];
+
 
   //icon variablaes
   const [bookToggle, setBookToggle] = useState(true);
+
+  function onMoreClick() {
+    if (pageNum < 165) {
+      pageNum += 6;
+      getMoreBooks();
+    } else {
+      console.log("No moreBooks in here");
+    }
+    console.log(moreBooks.length);
+  }
 
   //아이콘 변경 토글 함수
   function onChoiceBookToggle() {
@@ -33,6 +46,26 @@ function GoodBooks() {
     setBookToggle((bookToggle) => (bookToggle = true));
   }
   
+  // More Books 책 정보를 추가로 담아 올 함수
+  const getMoreBooks = async() => {
+    let bookList = [];
+    for(let i = 1; i<7; i++){
+      const json = await ( await fetch(
+        `${API_URL}?serviceKey=${SECRET_KEY}&numOfRows=${countBook}&pageNo=${pageNum + i}`,
+        {
+          headers: {
+            accept: "application/json",
+          },
+        }
+      )
+    ).json();
+      bookList.push(json.response.body.items.item);
+      console.log(bookList[i-1]);
+      moreBooks.push(bookList);
+    }
+  }
+
+
   //처음 렌더링 될 때 API를 읽어올 함수
   const getBooks = async () => {
     let bookList = [];
@@ -65,7 +98,6 @@ function GoodBooks() {
       )
     ).json();
     setBooks(json.response.body.items.item);
-    bookList = bookList.concat();
     setBookLists(bookList);
     console.log(bookList.concat());
     setLoading(false);
@@ -95,7 +127,7 @@ function GoodBooks() {
           <span onClick={onChoiceBookToggle}>선택도서</span>
         </div>
         <div className="GoodBooks__title--right">
-          <span className="GoodBooks__title__MoreBtn">More Books</span>
+          <span onClick={onMoreClick} className="GoodBooks__title__MoreBtn">More Books</span>
         </div>
       </div>
       {loading ? (
@@ -103,6 +135,7 @@ function GoodBooks() {
       ) : (
         <div>
           <View totalBooks={books} booksArray = {booklists} bookToggle={bookToggle} />
+          {/* <MoreBooks totalBooks={books} moreBooks = {moreBooks} bookToggle={bookToggle} /> */}
         </div>
       )}
     </div>
